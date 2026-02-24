a1b9c46041d575ac819d994c4f95905ff30d2e93
Bhuvana908 <bhuvanareddy908@gmail.com>
2026-02-24 20:45:41 +0530

    fix port for deployment

--- app.py
+++ app.py
@@ -177,5 +177,6 @@ def predict_endpoint():
         return jsonify({"error": "Internal server error during prediction."}), 500
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
